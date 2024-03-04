3e1fb81cb01a5bca4a2cd704aa2e17284facfa5b
mr-lovalova <47424670+mr-lovalova@users.noreply.github.com>
2024-03-04 11:20:02 +0100

    added abstract serializer class

--- cabinet_image_data_extraction/serializers.py
+++ cabinet_image_data_extraction/serializers.py
@@ -1,5 +1,6 @@
 import json
 from helpers import ObjectFactory
+from abc import ABC, abstractmethod
 
 
 class ObjectSerializer:
@@ -9,10 +10,24 @@ class ObjectSerializer:
         return serializer.to_str()
 
 
-class DictSerializer:
+class Serializer(ABC):
     def __init__(self):
         self._current_object = None
 
+    @abstractmethod
+    def start_object(self, object_name, object_id):
+        self._current_object = {"id": object_id}
+
+    @abstractmethod
+    def add_property(self, name, value):
+        self._current_object[name] = value
+
+    @abstractmethod
+    def to_str(self):
+        return self._current_object
+
+
+class DictSerializer(Serializer):
     def start_object(self, object_name, object_id):
         self._current_object = {"id": object_id}
 
