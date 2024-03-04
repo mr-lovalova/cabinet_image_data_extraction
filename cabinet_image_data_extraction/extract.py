da9ed722497e3ddc6ea170cf827461527caa7b34
mr-lovalova <47424670+mr-lovalova@users.noreply.github.com>
2024-03-04 11:21:20 +0100

    yolov8 functions

--- cabinet_image_data_extraction/extract.py
+++ cabinet_image_data_extraction/extract.py
@@ -1,6 +1,23 @@
 import item
 
 
+def get_crop(img, box):
+    x1, y1, x2, y2 = box
+    crop = img[int(y1) : int(y2), int(x1) : int(x2)]
+    return crop
+
+
+def from_img2(model, img, **ignored):
+    results = model(img)[0]
+    extractions = []
+    for detection in results.boxes:
+        prediction = model.names[int(detection.cls)]
+        crop = get_crop(results.orig_img, detection.xyxy[0])
+        extractions.append(item.factory.create(prediction, crop=crop, **ignored))
+    return extractions
+
+
+# YOLOv5 functionality
 def from_img(model, img, **ignored):
     detections = model(img).crop(save=False)
     extractions = []
